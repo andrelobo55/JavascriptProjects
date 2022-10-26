a1141a27b938a73879194b0e8d4769b18afc8eb6
André Lobo <78771870+andrelobo55@users.noreply.github.com>
2022-10-26 17:40:49 -0300

    Add files via upload

---
+++ aula11ex01.js
@@ -0,0 +1,8 @@
+/* console.log('O console está funcionando corretamente!')*/
+var vel = 61.5
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+if(vel > 60.5){ /*  Condição simples (tem apenas um if). */
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`) /* é executado quando a vel for maior que 60.5 */
+}
+console.log("Dirija sempre usando cinto de segurança!")
---
+++ aula11ex02.js
@@ -0,0 +1,8 @@
+var pais = "EUA"
+
+if(pais == "Brasil"){
+    console.log("Você é brasileira!")
+}
+else{
+    console.log("Você é estrangeira!")
+}
