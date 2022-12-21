faa768fac47b03344d660f944945c2a00179f301
André Lobo <78771870+andrelobo55@users.noreply.github.com>
2022-12-21 11:55:35 -0300

    Add files via upload

---
+++ aula14ex01.js
@@ -0,0 +1,13 @@
+var i = 0
+
+/**
+ * Laço de repetição for
+ */
+for(i; i < 12; i++) {
+    //console.log(`Passo: ${i+1}`)
+    mostrar(i)
+}
+
+function mostrar(i) {
+    console.log(`Passo: ${i+1}`)
+}
